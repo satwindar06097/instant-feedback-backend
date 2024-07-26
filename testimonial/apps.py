b025796bdd898161df5d90a395bc3b202f634203
satwindar06097 <satwindar06097@gmail.com>
2024-07-27 00:04:01 +0530

    fix-signal

--- testimonial/apps.py
+++ testimonial/apps.py
@@ -1,13 +1,8 @@
 from django.apps import AppConfig
 
-
 class TestimonialConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'testimonial'
 
     def ready(self):
-        import testimonial.signals
-
-    
-
-    
+        import testimonial.signals  # Ensure this line is present to load signals
--- testimonial/signals.py
+++ testimonial/signals.py
@@ -1,9 +1,22 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_delete
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from .models import Review
 
-def updateUser(sender,instance,**kwargs):
+# Existing signal to update the username to email
+@receiver(pre_save, sender=User)
+def updateUser(sender, instance, **kwargs):
     user = instance
     if user.email != '':
         user.username = user.email
 
-pre_save.connect(updateUser,sender=User)        
+# New signal to delete associated files when a review is deleted
+@receiver(post_delete, sender=Review)
+def delete_review_files(sender, instance, **kwargs):
+    if instance.customer_photo:
+        instance.customer_photo.delete(save=False)
+    if instance.review_image:
+        instance.review_image.delete(save=False)
+    if instance.video_review:
+        instance.video_review.delete(save=False)
